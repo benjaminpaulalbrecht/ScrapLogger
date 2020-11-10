aedf89b42377653cac70952ad9771d94cbb21cb5
Albrecht <Benjamin.Albrecht@energyTransfer.com>
2020-11-10 15:21:59 -0600

    added improved ScrapLogger

--- ScrapLogger.ps1
+++ ScrapLogger.ps1
@@ -1,38 +1,57 @@
-# ScrapLogger
-# todo: a build system for powershell scripts / general programs (scripts or compiled) called "Scrapper" can employ cvs/test framework 
+$symbols = [PSCustomObject] @{
+    SMILEY_WHITE = ([char]9786)
+    SMILEY_BLACK = ([char]9787)
+    GEAR         = ([char]9788)
+    HEART        = ([char]9829)
+    DIAMOND      = ([char]9830)
+    CLUB         = ([char]9827)
+    SPADE        = ([char]9824)
+    CIRCLE       = ([char]8226)
+    NOTE1        = ([char]9834)
+    NOTE2        = ([char]9835)
+    MALE         = ([char]9794)
+    FEMALE       = ([char]9792)
+    YEN          = ([char]165)
+    COPYRIGHT    = ([char]169)
+    PI           = ([char]960)
+    TRADEMARK    = ([char]8482)
+    CHECKMARK    = ([char]8730)
+}
 
-# default vars 
-$appId = "ScrapLogger!" 
-$LogFilesystem = "C:\users\balbrecht\Logs" 
-$logFile = "$LogFilesystem\log-(Get-Date).log"
+#Set Ansi Colors
+#todo: Set-ItemProperty HKCU:\Console VirtualTerminalLevel -Type DWORD 1
 
-function Write-Log() 
-{ 
-    [CmdletBinding()]
+function Scrap {
     param (
-        [Parameter(Mandatory = $true )]
-        [String]
-        $LogMessage
+        $message,
+        $loglevel
     )
 
-    # helper function: 
-    # setup your logging parameters here , and then just pass the LogMessage into here. 
-
-    if ($LogMessage = NULL ) { 
-        Write-Host "The Logging Message was empty! Please try again" 
-        exit -1  
-    } 
+    switch ($loglevel) { 
+        { $_ -eq "Info" } { 
+            $levelColor = "Magenta"
+            $stat = $($symbols.GEAR)
+        }
+        { $_ -eq "Error" } { 
+            $levelColor = "Red"
+            $stat = $($symbols.SMILEY_BLACK)
+        }
+        { $_ -eq "Warning" } {
+            $levelColor = "Orange"
+        }
+        { $_ -eq "Success" } { 
+            $levelColor = "Green"
+            $stat = $($symbols.CHECKMARK)
+        }
+        { $_ -eq "Failure" } { 
+            $levelColor = "Red"
+            $stat = $($symbols.FEMALE)
+        }
+    }
     
+    Write-Host "$stat  " -NoNewline -ForegroundColor Cyan # for Comments  
+    Write-Host "$(Get-Date): " -NoNewLine -BackGroundColor "Gray" -ForegroundColor "Black"
+    Write-Host " $message   " -NoNewline -ForegroundColor $levelColor
+    Write-Host "    " 
 }
-
-
-
-function main() {
-
-
-
-    Get-Content $logFile -Wait | ForEach-Object {Write-Host -ForegroundColor (Get-LogColor $_) $_}
-}
-
-main
 
